ba8302fa662de8c510685a140ae0e4af85cd9058
Wang Haoxuan <aqwanghaoxuan@163.com>
2021-10-26 07:33:40 +0800

    add RestTemplate call

--- cloud-demo/cloud-demo/order-service/src/main/java/cn/itcast/order/web/OrderController.java
+++ cloud-demo/cloud-demo/order-service/src/main/java/cn/itcast/order/web/OrderController.java
@@ -1,12 +1,14 @@
 package cn.itcast.order.web;
 
 import cn.itcast.order.pojo.Order;
+import cn.itcast.order.pojo.User;
 import cn.itcast.order.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("order")
@@ -14,10 +16,16 @@ public class OrderController {
 
    @Autowired
    private OrderService orderService;
+   @Autowired
+   private RestTemplate restTemplate;
 
     @GetMapping("{orderId}")
     public Order queryOrderByUserId(@PathVariable("orderId") Long orderId) {
         // 根据id查询订单并返回
-        return orderService.queryOrderById(orderId);
+        Order order = orderService.queryOrderById(orderId);
+        String url = "http://localhost:8081/user/" + order.getUserId();
+        User user = restTemplate.getForObject(url, User.class);
+        order.setUser(user);
+        return order;
     }
 }

0f3dc7261718db7e610b8d3ec8375dcb14600794
Wang Haoxuan <aqwanghaoxuan@163.com>
2021-10-30 15:09:24 +0800

    add nacos configuration
    fuck java

--- cloud-demo/cloud-demo/user-service/src/main/java/cn/itcast/user/web/UserController.java
+++ cloud-demo/cloud-demo/user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -1,10 +1,17 @@
 package cn.itcast.user.web;
 
+import cn.itcast.user.config.PatternProperties;
 import cn.itcast.user.pojo.User;
 import cn.itcast.user.service.UserService;
+import com.alibaba.nacos.api.config.annotation.NacosValue;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @RestController
@@ -14,6 +21,8 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private PatternProperties properties;
     /**
      * 路径： /user/110
      *
@@ -23,5 +32,10 @@ public class UserController {
     @GetMapping("/{id}")
     public User queryById(@PathVariable("id") Long id) {
         return userService.queryById(id);
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "return string is " + properties.getTestString();
     }
 }
